9629ef7d243b55638e406d9a16302dc6557cbc23
Grant Willison <emailgdw@gmail.com>
2022-09-14 21:08:57 -0700

    read and write to files

--- deck.go
+++ deck.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // create type deck
 // ..which is slice of strings
 
 type deck []string
 
-func (d deck) print() {
+func (d deck) print() { //reciever on deck type
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
@@ -26,6 +30,23 @@ func newDeck() deck {
 	return cards
 }
 
-func deal(d deck, handSize int) (deck, deck) {
+func deal(d deck, handSize int) (deck, deck) { // takes deck as argument
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+func (d deck) saveToFile(fileName string) error {
+	return os.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+func newDeckFromFile(fileName string) deck {
+	bSlice, err := os.ReadFile(fileName)
+	if err != nil {
+
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bSlice), ",")
+	return deck(s)
 }
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 func main() {
-	cards := newDeck()
-	hand, remainingDeck := deal(cards, 5)
-	hand.print()
-	remainingDeck.print()
+	// cards := newDeck()
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_card")
+	cards.print()
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.print()
+	// remainingDeck.print()
+
+	// greeting := "hi there"
+	// fmt.Println([]byte(greeting))
 }
 
 func newCard() string {
