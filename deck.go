58ee942eeca0bc73c612db61bfe4ec5ef8c750fb
Grant Willison <emailgdw@gmail.com>
2022-09-14 18:05:42 -0700

    print a deck of cards

---
+++ deck.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// create type deck
+// ..which is slice of strings
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "two", "three", "four", "five"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
